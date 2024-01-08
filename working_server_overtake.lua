1fc577816a9a8f2f4098751b0d50258690f80626
Rusty Mann <mannrusty@gmail.com>
2024-01-07 17:49:15 -0800

    adding latest edits, probably broken

--- working_server_overtake.lua
+++ working_server_overtake.lua
@@ -1,5 +1,6 @@
 
 local requiredSpeed = 40
+local countDown = 90
 
 function kmhtomph(kmh)
     return math.ceil(kmh * 0.621)
@@ -12,7 +13,8 @@ function script.prepare(dt)
     ac.debug("speed", ac.getCarState(1).speedKmh)
     return ac.getCarState(1).speedKmh > requiredSpeed
 end
- 
+
+
 local timePassed = 0
 local totalScore = 0
 local comboMeter = 1
@@ -23,7 +25,7 @@ local carsState = {}
 local wheelsWarningTimeout = 0
 -- local DriftTracking = ac.getCarState(1)
 local stored = { }
- 
+
 stored.playerscore = ac.storage('playerscore', highestScore) --default value
 highestScore = stored.playerscore:get()
 ac.sendChatMessage("has a highscore of " .. highestScore .. " pts.")
@@ -49,6 +51,7 @@ function script.update(dt)
     end
  
     timePassed = timePassed + dt
+    countDown = countDown - dt
  
     local comboFadingRate = 0.1 * math.lerp(1, 0.1, math.lerpInvSat(player.speedKmh, 80, 200)) + player.wheelsOutside/4
     comboMeter = math.max(1, comboMeter - dt * comboFadingRate)
@@ -84,8 +87,8 @@ function script.update(dt)
         dangerouslySlowTimer = 0
     end
  
-    for i = 1, ac.getSimState().carsCount do
-        local car = ac.getCarState(i)
+    for i = 2, ac.getSimState().carsCount do
+        local car = ac.getCar(i - 1) or error()
         local state = carsState[i]
  
         if car.pos:closerToThan(player.pos, 10) then
@@ -98,36 +101,29 @@ function script.update(dt)
 
                     if car.pos:closerToThan(player.pos, 2.5) then
                         comboMeter = comboMeter + 3
-                        addMessage("Very close near miss!", 1)
+                        addMessage('Very close near miss!', 1)
                     else
                         comboMeter = comboMeter + 1
-                        addMessage("Near miss: bonus combo", 0)
+                        addMessage('Near miss: bonus combo', 0)
                     end
                 end
             end
  
-            if car.collidedWith then
-                addMessage("Collision!", -1)
+            if car.collidedWith == 0 and not state.collided then
+                addMessage('Collision!', -1)
                 state.collided = true
  
         		if totalScore > highestScore then
             		highestScore = math.floor(totalScore)
             		stored.playerscore:set(highestScore)
-            ac.sendChatMessage("has a NEW highscore of " .. totalScore .. " pts.")
+                    ac.sendChatMessage("has a NEW highscore of " .. totalScore .. " pts.")
         		end
                 totalScore = 0
                 comboMeter = 1
             end
- 
-            -- if DriftTracking.isDriftValid and player.wheelsOutside < 3 then
-            --     totalScore = totalScore + math.ceil(1 * comboMeter)
-            --     comboMeter = comboMeter + 0.02
-            --     comboColor = comboColor + 5
-            --     addMessage('Drift It!', 1)
-            -- end
- 
+
             if not state.overtaken and not state.collided and state.drivingAlong then
-                local posDir = (car.pos - player.pos):normalize()
+                local posDir = (car.position - player.position):normalize()
                 local posDot = math.dot(posDir, car.look)
                 state.maxPosDot = math.max(state.maxPosDot, posDot)
                 if posDot < -0.5 and state.maxPosDot > 0.5 then
@@ -135,6 +131,7 @@ function script.update(dt)
                     addMessage('Overtake! + '..(math.ceil(10 * comboMeter))..' pts', comboMeter > 20 and 1 or 0)
                     comboMeter = comboMeter + 1
                     comboColor = comboColor + 10
+                    countDown = countDown + 10
                     state.overtaken = true
                 end
             end
@@ -211,72 +208,82 @@ local function updateMessages(dt)
 end
 
 local speedWarning = 0
-    function script.drawUI()
-        local uiState = ac.getUiState()
-        updateMessages(uiState.dt)
- 
-        local speedRelative = math.saturate(math.floor(ac.getCarState(1).speedKmh) / requiredSpeed)
-        speedWarning = math.applyLag(speedWarning, speedRelative < 1 and 1 or 0, 0.5, uiState.dt)
- 
-        local colorDark = rgbm(0.4, 0.4, 0.4, 1)
-        local colorGrey = rgbm(0.7, 0.7, 0.7, 1)
-        local colorAccent = rgbm.new(hsv(speedRelative * 120, 1, 1):rgb(), 1)
-        local colorCombo =
-            rgbm.new(hsv(comboColor, math.saturate(comboMeter / 10), 1):rgb(), math.saturate(comboMeter / 4))
-        local function speedMeter(ref)
-            ui.drawRectFilled(ref + vec2(0, -4), ref + vec2(180, 5), colorDark, 1)
-            ui.drawLine(ref + vec2(0, -4), ref + vec2(0, 4), colorGrey, 1)
-            ui.drawLine(ref + vec2(requiredSpeed, -4), ref + vec2(requiredSpeed, 4), colorGrey, 1)
- 
-            local speed = math.min(ac.getCarState(1).speedKmh, 180)
-            if speed > 1 then
-                ui.drawLine(ref + vec2(0, 0), ref + vec2(speed, 0), colorAccent, 4)
-            end
+function script.drawUI()
+    local uiState = ac.getUiState()
+    updateMessages(uiState.dt)
+
+    local speedRelative = math.saturate(math.floor(ac.getCarState(1).speedKmh) / requiredSpeed)
+    speedWarning = math.applyLag(speedWarning, speedRelative < 1 and 1 or 0, 0.5, uiState.dt)
+
+    local colorDark = rgbm(0.4, 0.4, 0.4, 1)
+    local colorGrey = rgbm(0.7, 0.7, 0.7, 1)
+    local colorAccent = rgbm.new(hsv(speedRelative * 120, 1, 1):rgb(), 1)
+    local colorCombo =
+        rgbm.new(hsv(comboColor, math.saturate(comboMeter / 10), 1):rgb(), math.saturate(comboMeter / 4))
+    local function speedMeter(ref)
+        ui.drawRectFilled(ref + vec2(0, -4), ref + vec2(180, 5), colorDark, 1)
+        ui.drawLine(ref + vec2(0, -4), ref + vec2(0, 4), colorGrey, 1)
+        ui.drawLine(ref + vec2(requiredSpeed, -4), ref + vec2(requiredSpeed, 4), colorGrey, 1)
+
+        local speed = math.min(ac.getCarState(1).speedKmh, 180)
+        if speed > 1 then
+            ui.drawLine(ref + vec2(0, 0), ref + vec2(speed, 0), colorAccent, 4)
         end
-        ui.beginTransparentWindow("overtakeScore", vec2(100, 100), vec2(400 * 0.5, 400 * 0.5))
-        ui.beginOutline()
- 
-        ui.pushStyleVar(ui.StyleVar.Alpha, 1 - speedWarning)
-        ui.pushFont(ui.Font.Main)
-        ui.text("Cruisin AC Overtake Run")
-        ui.text("Highest Score: " .. highestScore .. " pts")
-        ui.popFont()
-        ui.pushFont(ui.Font.Title)
-        ui.text(totalScore .. " pts")
-        ui.sameLine(0, 20)
-        ui.beginRotation()
-        ui.textColored(math.ceil(comboMeter * 10) / 10 .. "x", colorCombo)
-        if comboMeter > 20 then
-            ui.endRotation(math.sin(comboMeter / 180 * 3141.5) * 3 * math.lerpInvSat(comboMeter, 20, 30) + 90)
-        end
-        ui.popFont()
-        ui.popStyleVar()
- 
-        ui.endOutline(rgbm(0, 0, 0, 0.3))
- 
-        ui.offsetCursorY(20)
-        ui.pushFont(ui.Font.Main)
-        local startPos = ui.getCursor()
-        for i = 1, #messages do
-            local m = messages[i]
-            local f = math.saturate(4 - m.currentPos) * math.saturate(8 - m.age)
-            ui.setCursor(startPos + vec2(20 * 0.5 + math.saturate(1 - m.age * 10) ^ 2 * 50, (m.currentPos - 1) * 15))
-            ui.textColored(
-                m.text,
-                m.mood == 1 and rgbm(0, 1, 0, f) or m.mood == -1 and rgbm(1, 0, 0, f) or rgbm(1, 1, 1, f)
-            )
-        end
-        ui.popFont()
-        ui.setCursor(startPos + vec2(0, 4 * 30))
- 
-        ui.pushStyleVar(ui.StyleVar.Alpha, speedWarning)
-        ui.setCursorY(0)
-        ui.pushFont(ui.Font.Main)
-        ui.textColored("Keep speed above " .. displaySpeed .. " mph:", colorAccent)
-        speedMeter(ui.getCursor() + vec2(-9 * 0.5, 4 * 0.2))
- 
-        ui.popFont()
-        ui.popStyleVar()
- 
-        ui.endTransparentWindow()
     end
+    ui.beginTransparentWindow("overtakeScore", vec2(uiState.windowSize.x * 0.5 - 600, 100), vec2(800, 800), false)
+    ui.beginOutline()
+
+    ui.pushStyleVar(ui.StyleVar.Alpha, 1 - speedWarning)
+    ui.pushFont(ui.Font.Main)
+    ui.text("Cruisin AC OutRun")
+    ui.text("Highest Score: " .. highestScore .. " pts")
+    ui.popFont()
+    ui.pushFont(ui.Font.Title)
+    ui.text(totalScore .. " pts")
+    ui.sameLine(0, 20)
+    ui.beginRotation()
+    ui.textColored(math.ceil(comboMeter * 10) / 10 .. "x", colorCombo)
+    if comboMeter > 20 then
+        ui.endRotation(math.sin(comboMeter / 180 * 3141.5) * 3 * math.lerpInvSat(comboMeter, 20, 30) + 90)
+    end
+    ui.popFont()
+    ui.popStyleVar()
+
+    ui.endOutline(rgbm(0, 0, 0, 0.3))
+
+    ui.offsetCursorY(20)
+    ui.pushFont(ui.Font.Main)
+    local startPos = ui.getCursor()
+    for i = 1, #messages do
+        local m = messages[i]
+        local f = math.saturate(4 - m.currentPos) * math.saturate(8 - m.age)
+        ui.setCursor(startPos + vec2(20 * 0.5 + math.saturate(1 - m.age * 10) ^ 2 * 50, (m.currentPos - 1) * 15))
+        ui.textColored(
+            m.text,
+            m.mood == 1 and rgbm(0, 1, 0, f) or m.mood == -1 and rgbm(1, 0, 0, f) or rgbm(1, 1, 1, f)
+        )
+    end
+    ui.popFont()
+    ui.setCursor(startPos + vec2(0, 4 * 30))
+
+    ui.pushStyleVar(ui.StyleVar.Alpha, speedWarning)
+    ui.setCursorY(0)
+    ui.pushFont(ui.Font.Main)
+    ui.textColored("Keep speed above " .. displaySpeed .. " mph:", colorAccent)
+    speedMeter(ui.getCursor() + vec2(-9 * 0.5, 4 * 0.2))
+
+    ui.popFont()
+    ui.popStyleVar()
+
+    ui.endTransparentWindow()
+
+    ui.beginTransparentWindow("overtakeScore", vec2(uiState.windowSize.x * 0.5, 100), vec2(400, 400), false)
+    ui.beginOutline()
+    ui.pushStyleVar(ui.StyleVar.Alpha, 1 - speedWarning)
+    ui.pushFont(ui.Font.Huge)
+    ui.text("Time: " ..math.floor(countDown).. "secs")
+
+    ui.endTransparentWindow()
+
+
+end
